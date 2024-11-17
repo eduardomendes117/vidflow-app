a8db9d9184a40b43fe99ac3841148e179ee4609b
DuduDev <107651217+eduardomendes117@users.noreply.github.com>
2024-11-17 09:19:05 -0300

    update

--- components/home.js
+++ components/home.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Home = () => {
   return (
-    <section className="max-w-screen-sm h-svh flex flex-col justify-center gap-5 p-5">
+    <section className="max-w-screen-sm h-svh flex flex-col justify-center gap-3 lg:gap-5 p-5">
       <h1 className="text-3xl text-center md:text-4xl lg:text-5xl xl:text-6xl font-medium lg:leading-tight">
         Automatize Suas Postagens de Vídeo em Múltiplas Plataformas com
         Facilidade
@@ -15,7 +15,7 @@ const Home = () => {
 
       <Link
         className="w-max rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-blue-500 text-background gap-2 hover:bg-blue-400 text-sm sm:text-2xl h-10 sm:h-14 px-4 sm:px-8 mx-auto"
-        href="/inicio"
+        href="/login"
         rel="noopener noreferrer"
       >
         Começar
